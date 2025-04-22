634c2dab31f3ad96c2024a911a8d38861fe98d66
Michael Seaton <mseaton@pih.org>
2025-04-22 19:44:35 -0400

    Delegate to HFE method for saving form from xml attributes

--- api/src/main/java/org/openmrs/module/htmlformentryui/HtmlFormUtil.java
+++ api/src/main/java/org/openmrs/module/htmlformentryui/HtmlFormUtil.java
@@ -14,28 +14,20 @@
 
 package org.openmrs.module.htmlformentryui;
 
-import org.apache.commons.lang3.StringUtils;
 import org.openmrs.Encounter;
-import org.openmrs.EncounterType;
-import org.openmrs.Form;
 import org.openmrs.Patient;
 import org.openmrs.Visit;
 import org.openmrs.api.FormService;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.htmlformentry.HtmlForm;
 import org.openmrs.module.htmlformentry.HtmlFormEntryService;
-import org.openmrs.module.htmlformentry.HtmlFormEntryUtil;
+import org.openmrs.module.htmlformentry.HtmlFormFromFileLoader;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.resource.ResourceFactory;
-import org.openmrs.util.OpenmrsUtil;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
 import java.io.IOException;
 
-/**
- *
- */
 public class HtmlFormUtil {
 	
 	public static HtmlForm getHtmlFormFromUiResource(ResourceFactory resourceFactory, FormService formService,
@@ -89,80 +81,8 @@ public class HtmlFormUtil {
 	
 	public static HtmlForm getHtmlFormFromResourceXml(FormService formService, HtmlFormEntryService htmlFormEntryService,
 	        String xml) {
-		try {
-			Document doc = HtmlFormEntryUtil.stringToDocument(xml);
-			Node htmlFormNode = HtmlFormEntryUtil.findChild(doc, "htmlform");
-			String formUuid = getAttributeValue(htmlFormNode, "formUuid");
-			if (formUuid == null) {
-				throw new IllegalArgumentException("formUuid is required");
-			}
-			Form form = formService.getFormByUuid(formUuid);
-			boolean needToSaveForm = false;
-			if (form == null) {
-				form = new Form();
-				form.setUuid(formUuid);
-				needToSaveForm = true;
-			}
-			
-			String formName = getAttributeValue(htmlFormNode, "formName");
-			if (!OpenmrsUtil.nullSafeEquals(form.getName(), formName)) {
-				form.setName(formName);
-				needToSaveForm = true;
-			}
-			
-			String formDescription = getAttributeValue(htmlFormNode, "formDescription");
-			if (!OpenmrsUtil.nullSafeEquals(form.getDescription(), formDescription)) {
-				form.setDescription(formDescription);
-				needToSaveForm = true;
-			}
-			
-			String formVersion = getAttributeValue(htmlFormNode, "formVersion");
-			if (!OpenmrsUtil.nullSafeEquals(form.getVersion(), formVersion)) {
-				form.setVersion(formVersion);
-				needToSaveForm = true;
-			}
-			
-			String formEncounterType = getAttributeValue(htmlFormNode, "formEncounterType");
-			EncounterType encounterType = formEncounterType == null ? null
-			        : HtmlFormEntryUtil.getEncounterType(formEncounterType);
-			if (encounterType != null && !OpenmrsUtil.nullSafeEquals(form.getEncounterType(), encounterType)) {
-				form.setEncounterType(encounterType);
-				needToSaveForm = true;
-			}
-			
-			if (needToSaveForm) {
-				formService.saveForm(form);
-			}
-			
-			HtmlForm htmlForm = htmlFormEntryService.getHtmlFormByForm(form);
-			boolean needToSaveHtmlForm = false;
-			if (htmlForm == null) {
-				htmlForm = new HtmlForm();
-				htmlForm.setForm(form);
-				needToSaveHtmlForm = true;
-				
-			}
-			
-			// if there is a html form uuid specified, make sure the htmlform uuid is set to that value
-			String htmlformUuid = getAttributeValue(htmlFormNode, "htmlformUuid");
-			if (StringUtils.isNotBlank(htmlformUuid) && !OpenmrsUtil.nullSafeEquals(htmlformUuid, htmlForm.getUuid())) {
-				htmlForm.setUuid(htmlformUuid);
-				needToSaveHtmlForm = true;
-			}
-			
-			if (!OpenmrsUtil.nullSafeEquals(trim(htmlForm.getXmlData()), trim(xml))) { // trim because if the file ends with a newline the db will have trimmed it
-				htmlForm.setXmlData(xml);
-				needToSaveHtmlForm = true;
-			}
-			if (needToSaveHtmlForm) {
-				htmlFormEntryService.saveHtmlForm(htmlForm);
-			}
-			return htmlForm;
-			
-		}
-		catch (Exception e) {
-			throw new IllegalArgumentException("Failed to parse XML and build Form and HtmlForm", e);
-		}
+		HtmlFormFromFileLoader loader = Context.getRegisteredComponents(HtmlFormFromFileLoader.class).get(0);
+		return loader.saveHtmlForm(xml);
 	}
 	
 	public static String determineReturnUrl(String returnUrl, String returnProviderName, String returnPageName,
@@ -207,14 +127,4 @@ public class HtmlFormUtil {
 		}
 		
 	}
-	
-	private static String trim(String s) {
-		return s == null ? null : s.trim();
-	}
-	
-	private static String getAttributeValue(Node htmlForm, String attributeName) {
-		Node item = htmlForm.getAttributes().getNamedItem(attributeName);
-		return item == null ? null : item.getNodeValue();
-	}
-	
 }
